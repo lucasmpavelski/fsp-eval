4a26be3af341324e9de106318f28c06027e3abd8
Lucas Marcondes Pavelski <lmpavelski@yahoo.com.br>
2021-01-01 11:00:12 -0300

    Adds: header only configuration.

--- conanfile.py
+++ conanfile.py
@@ -4,35 +4,25 @@ from conans import ConanFile, CMake, tools
 class FspEvalConan(ConanFile):
     name = "fsp-eval"
     version = "0.1"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
+    license = "LICENCE"
+    author = "Lucas Marcondes Pavelski <lpavelski@alunos.utfpr.edu.br>"
+    url = ""
     description = "Flowshop evaluation methods"
     topics = ("flowshop", "permutation", "optimization")
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
-    exports_sources = "src/*", "cmake/*", "CMakeLists.txt", "test/*"
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+    
+    # No settings/options are necessary, this is header only
+    exports_sources = "src/*"
+    no_copy_source = True
 
     def build(self):
+        pass
 
-        line_to_replace = 'include(cmake/StandardProjectSettings.cmake)'
-        tools.replace_in_file("CMakeLists.txt", line_to_replace,
-                              '''{}
-include("{}/conanbuildinfo.cmake")
-conan_basic_setup()'''.format(line_to_replace, self.install_folder.replace("\\", "/")))
-
-        cmake = CMake(self)
-        cmake.definitions["ENABLE_TESTING"] = "OFF"
-        cmake.configure(source_folder=".")
-        cmake.build()
-        
     def package(self):
-        self.copy("*.h", dst="include", src="src")
+        self.copy("*.hpp", dst="include", src="src")
+
+    def package_id(self):
+        self.info.header_only()
 
     def package_info(self):
-        self.cpp_info.libs = ["fsp-eval"]
-
+        self.cpp_info.name = "fsp-eval"
+        self.cpp_info.includedirs = ['include']
